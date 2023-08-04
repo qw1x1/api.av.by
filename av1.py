1af56193312bc73c80339982ba8f794a9af69670
qw1x1 <qw1x.stars@gmail.com>
2023-08-04 14:08:19 +0300

    INIT

---
+++ av1.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+brand_id = 683 # mersedec-benz
+model_id = 5879 # C-class
+generations_id = 4508 # 202 - 1 generations
+
+class Select_car: # Fills in files: brand, model, generations.
+    def __init__(self) -> None:
+        self.brand_id = 0
+        self.model_id = 0
+        self.generations_id = 0
+
+    def get_brand_car_list(self):
+        brand_list = requests.get('https://api.av.by/offer-types/cars/catalog/brand-items')
+        data_brand = json.loads(brand_list.text)
+        with open('brand.json', 'w') as brand:
+            json.dump(data_brand, brand)
+            
+    def get_model_car_list(self):
+        model_list = requests.get('https://api.av.by/offer-types/cars/catalog/brand-items/'+ str(brand_id) +'/models')
+        data_model = json.loads(model_list.text)
+        with open('model.json', 'w') as model:
+            json.dump(data_model, model)
+
+    def get_generations_car_list(self):
+        generations_list = requests.get('https://api.av.by/offer-types/cars/catalog/brand-items/'+ str(brand_id) +'/models/'+ str(model_id) +'/generations')
+        data_generations = json.loads(generations_list.text)
+        with open('generations.json', 'w') as generations:
+            json.dump(data_generations, generations)
+
+    def __call__(self):
+        self.get_brand_car_list()
+        self.get_model_car()
+        self.get_generations_car()
+
+# obj = Select_car()
+# obj()
+
+
+def get_page_car(brand_id, model_id, generations_id):
+    page_htm = requests.get('https://cars.av.by/filter?', {'brands[0][brand]': brand_id, 'brands[0][model]': model_id, 'brands[0][generation]': generations_id, 'condition[0]': 2, 'sort': 2})
+    if page_htm.status_code == 200:
+        with open('page.htm', 'w', encoding="utf-8") as htm:
+            data = htm.write(page_htm.text)
+
+get_page_car(brand_id, model_id, generations_id)
---
+++ test.py
@@ -0,0 +1,52 @@
+import requests 
+import json
+
+# car_dict = {}
+
+# # Находит обьявление и если оно активно, то добавляет в car_dict.
+# def get_car_by_id(id):
+#     data = requests.get('https://api.av.by/offers/' + str(id))
+#     car_obj = json.loads(data.text)
+#     if 'publicUrl' in car_obj and car_obj['publicStatus']['name'] == 'active':
+#         return car_obj
+#     else:
+#         return 'NANE'
+    
+# # Перебераем id и кидаем в get_car_by_id
+# for id in range(100757688 , 100757699):
+#     if get_car_by_id(id) != 'NANE':
+#         # print(get_car_by_id(id)['properties']) locationName
+#         car_dict[str(id)] = get_car_by_id(id)['properties'][0]['value'], get_car_by_id(id)['publicUrl'], get_car_by_id(id)['properties'][0]['value'], get_car_by_id(id)['properties'][1]['value'],get_car_by_id(id)['properties'][2]['value'], get_car_by_id(id)['properties'][3]['value'], get_car_by_id(id)['properties'][-1]['value'], get_car_by_id(id)['properties'][-3]['value'],get_car_by_id(id)['properties'][6]['value'], get_car_by_id(id)['properties'][4]['value'], get_car_by_id(id)['properties'][5]['value'], get_car_by_id(id)['locationName']
+
+# # Записываем в файл все активные обьявления
+# with open('car.json', 'w') as car:
+#     json.dump(car_dict, car)
+
+
+# # with open('car.json') as json_file:
+# #    data = (json.load(json_file))
+# # for id in data:  
+# #     print(data[id][1], data[id][2], data[id][3], data[id][4], data[id][5])
+# #     print('Цена:', data[id][6])  
+# #     print('Пробег:', data[id][7]) 
+# #     print('Ходовая:', data[id][8], data[id][9], data[id][10]) 
+# #     print('Местоположение:', data[id][11])
+# #     print()
+
+# try:
+#     brand = byUsrUrlObj.read()
+#     qUserData = json.loads(brand).decode('utf-8')
+#     questionSubjs = qUserData["all"]["questions"]
+# except ValueError:  # includes simplejson.decoder.JSONDecodeError
+    # print('Decoding JSON has failed')
+# with open('brand.json', encoding="utf-8") as json_file:
+#    data = (json.load(json_file))
+
+with open('brand.json', 'r') as brand:
+    data_brand = json.load(brand)
+    for i in range(len(data_brand)):
+        print(data_brand[i]['id'], data_brand[i]['name'])
+    id = int(input())
+    print(data_brand[id])
+
+
