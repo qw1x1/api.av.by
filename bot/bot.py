2ee0c6299f6d2e803a713a90573508fcb9558462
MANSUXA228 <141494937+MANSUXA228@users.noreply.github.com>
2023-08-12 01:29:41 +0300

    Add files via upload

--- bot/bot.py
+++ bot/bot.py
@@ -3,7 +3,8 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config_reader import config
-from aiogram.types import BotCommand
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters import Text
 import json, io
 
 
@@ -24,19 +25,33 @@ dp = Dispatcher()
 #    await message.answer("Hello!")
 
 def list_add():
-    cars=[]
+    cars=InlineKeyboardBuilder()
     with io.open("brand.json",encoding='utf-8') as js_file:
         list=json.load(js_file)
-
-        for country in list:
-            cars.append(types.BotCommand(command=country['slug'].replace('-',''),description=country['name']))
+        for key, value in list.items():
+            cars.adjust(3)
+            cars.add(types.InlineKeyboardButton(
+                text=key,
+                callback_data=value)
+                )
     return cars
-    
-
 # Запуск процесса поллинга новых апдейтов
+
+builder = InlineKeyboardBuilder()
+
+@dp.message(Command("start"))
+async def call_backs(message: types.Message):
+    builder = list_add()
+    await message.answer(
+        "Выберите бренд автомобиля",
+        reply_markup=builder.as_markup()
+    )
+
+@dp.callback_query(Text(startswith=""))
+async def send_random_value(callback: types.CallbackQuery):
+    await callback.message.answer("ANUS")
+
 async def main():
-    cars=list_add()
-    await bot.set_my_commands(cars)
     await dp.start_polling(bot)
     
 if __name__ ==  '__main__':
