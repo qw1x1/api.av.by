35be5713f0c57c501d0ac82edad522ad72f593c8
qw1x1 <qw1x.stars@gmail.com>
2023-08-18 11:21:21 +0300

    asd

--- test.py
+++ test.py
@@ -7,6 +7,164 @@ import json
 import math
 
 
+brand = {
+    "Abarth": 10297,
+    "Acura": 1444,
+    "Alfa Romeo": 1,
+    "Alpina": 5940,
+    "ARO": 5324,
+    "Asia": 5772,
+    "Aston Martin": 2325,
+    "Audi": 6,
+    "Avatr": 10346,
+    "BAIC ": 10034,
+    "Baojun": 10337,
+    "Bentley": 1676,
+    "BMW": 8,
+    "Brilliance": 2210,
+    "Buick": 1506,
+    "BYD": 5459,
+    "Cadillac": 40,
+    "Changan": 2632,
+    "Chery": 1998,
+    "Chevrolet": 41,
+    "Chrysler": 42,
+    "Citroen": 43,
+    "Cupra": 10236,
+    "Dacia": 1841,
+    "Daewoo": 46,
+    "Daihatsu": 47,
+    "Datsun": 2578,
+    "Denza": 10287,
+    "Derways": 5109,
+    "Dodge": 45,
+    "Dongfeng": 5780,
+    "Dongfeng Honda": 10106,
+    "Eagle": 10240,
+    "Everus": 10224,
+    "EXEED": 10303,
+    "FAW": 2465,
+    "Ferrari": 288,
+    "Fiat": 301,
+    "Fisker": 2323,
+    "Ford": 330,
+    "Foton": 2355,
+    "FSO": 5526,
+    "GAC": 10131,
+    "GAC Honda": 10222,
+    "Geely": 2012,
+    "Genesis": 10006,
+    "GMC": 372,
+    "Gonow": 6005,
+    "Great Wall": 1726,
+    "Hafei": 2215,
+    "Haima": 5070,
+    "Haval": 5782,
+    "HiPhi": 10279,
+    "Honda": 383,
+    "Hongqi": 10275,
+    "Hongxing": 2681,
+    "Hozon": 10259,
+    "Hummer": 1498,
+    "Hycan": 10326,
+    "Hyundai": 433,
+    "Infiniti": 1343,
+    "Iran Khodro": 2022,
+    "Isuzu": 461,
+    "JAC": 2030,
+    "Jaguar": 526,
+    "Jeep": 540,
+    "Jetour": 10362,
+    "Jiangling": 2272,
+    "Joylong": 10188,
+    "Kandi": 10238,
+    "Kia": 545,
+    "Lada (ВАЗ)": 1279,
+    "Lamborghini": 2437,
+    "Lancia": 572,
+    "Land Rover": 584,
+    "Leapmotor": 10268,
+    "Lexus": 589,
+    "Lifan": 2586,
+    "Lincoln": 601,
+    "LiXiang": 10209,
+    "Lotus": 2295,
+    "Mahindra": 5957,
+    "Maserati": 1625,
+    "Maybach": 2169,
+    "Mazda": 634,
+    "McLaren": 5970,
+    "Mercedes-Benz": 683,
+    "Mercury": 825,
+    "MG": 1906,
+    "Microcar": 10090,
+    "MINI": 1850,
+    "Mitsubishi": 834,
+    "Morgan": 5937,
+    "Nissan": 892,
+    "Oldsmobile": 1364,
+    "Opel": 966,
+    "Peugeot": 989,
+    "Piaggio": 2221,
+    "Plymouth": 1012,
+    "Polestar": 10042,
+    "Pontiac": 1022,
+    "Porsche": 1485,
+    "Proton": 1609,
+    "RAM": 10226,
+    "Ravon": 5503,
+    "Renault": 1039,
+    "Renault Samsung": 10100,
+    "Roewe": 5800,
+    "Rolls-Royce": 5100,
+    "Rover": 1067,
+    "Saab": 1085,
+    "Saipa": 5029,
+    "Santana": 5517,
+    "Saturn": 1703,
+    "Scion": 2698,
+    "SEAT": 1091,
+    "Seres": 10289,
+    "Shanghai Maple": 5822,
+    "Skoda": 1126,
+    "Skywell": 10308,
+    "Smart": 2449,
+    "SRM": 10350,
+    "SsangYong": 1597,
+    "Subaru": 1136,
+    "Suzuki": 1155,
+    "Tata": 5447,
+    "Tatra": 10161,
+    "Tesla": 2521,
+    "Think": 10013,
+    "Tianma": 5520,
+    "Toyota": 1181,
+    "Trabant": 5080,
+    "VGV": 10368,
+    "Volkswagen": 1216,
+    "Volvo": 1238,
+    "Vortex": 5437,
+    "Voyah": 10244,
+    "Wartburg": 1857,
+    "Weltmeister": 10067,
+    "Wuling": 10334,
+    "Xpeng": 6019,
+    "Yudo": 10285,
+    "Zeekr": 10185,
+    "Zotye": 2510,
+    "ZX": 5066,
+    "Богдан": 5076,
+    "ГАЗ": 1310,
+    "ЕрАЗ": 10094,
+    "ЗАЗ": 1551,
+    "ИЖ": 2894,
+    "ЛуАЗ": 2345,
+    "Москвич": 2051,
+    "РАФ": 5252,
+    "ТагАЗ": 5032,
+    "УАЗ": 1464,
+    "Эксклюзив": 5019
+}
 # car_dict = {}
 
 # # Находит обьявление и если оно активно, то добавляет в car_dict.
@@ -431,11 +589,49 @@ import math
 
 
 
-def get_started(brand_dict, year_min, year_max, price_min, price_max, deviation_procent):
-    obj = Select_car(brand_dict)
-    car_crit = obj()
 
-    car_odj = Pars_info_id_file(year_min=year_min, year_max=year_max, price_min=price_min, price_max=price_max, brand_id = car_crit[0], model_id = car_crit[1])
-    car_list_odj = car_odj()
-    # 
-    serch_car_odj = Search_cars(car_list=car_list_odj[0], count_page=car_list_odj[1], deviation_procent = deviation_procent)
+class Select_car: # -> brand_id, model_id
+    def __init__(self, brand, metod) -> None:
+        self.brand_id = 0
+        self.model_id = 0
+        self.brand = brand
+        self.model = {}
+        self.user = User().random
+        self.metod = metod
+
+    # Пока в консоль :)
+    @staticmethod
+    def output_car(data): # -> id
+        for key, value in data.items():
+            print(key, value)
+        print()
+        key = str(input())
+        return data[key]
+
+    def get_data_select_car(self, params):
+        respons_list = requests.get('https://api.av.by/offer-types/cars/catalog/brand-items/' + params, headers={'user-agent': f'{self.user}'})
+        if respons_list.status_code == 200:
+            respons_data = json.loads(respons_list.text)
+            for i in range(len(respons_data)):
+                self.model[respons_data[i]['name']] = respons_data[i]['id']
+
+    def __call__(self): # -> brand_id, model_id
+        self.brand_id = self.metod(self.brand)
+        self.get_data_select_car(str(self.brand_id) +'/models')
+        self.model_id = self.metod(self.model)
+        return self.brand_id, self.model_id
+    
+
+
+
+def output_car(data): 
+    # Напишеш функцию для вывода и получения id, тип этой ток не в консоль а в тг_бот и предаш ее 2 параметрам классу Select_car(brand=brand, metod='''''ТУТ'''')
+    for key, value in data.items():
+        print(key, value)
+    print()
+    key = str(input())
+    return data[key]
+
+obj = Select_car(brand=brand, metod=output_car)
+ger = obj()
+print(ger)
