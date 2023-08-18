c0b5c674e953b240d91a240902549fa779532151
qw1x1 <qw1x.stars@gmail.com>
2023-08-18 16:41:20 +0300

    Ну просто пизда

--- api/controls.py
+++ api/controls.py
@@ -1,44 +1,52 @@
-from models import User, Request
+from models import User, Request, db
 
-def create_user(telegram_id):
-    '''
-    Если пользователь уже создан return User
-    '''
-    if User.get(User.telegram_id == telegram_id):
-        return User.get(User.telegram_id == telegram_id)
-    else:
-        User.create(telegram_id=telegram_id)
-        return User.get(User.telegram_id == telegram_id)
+class Control_db():
+    def __init__(self, telegram_id):
+        self.telegram_id = telegram_id
+        self.requests_list = []
 
-def create_request(brand_id, model_id, percent_difference, year_min, year_max, price_min, price_max, user):
-    '''
-    Добавляем новые данные поиска для User
-    '''
-    Request.create(
-        brand_id=brand_id,
-        model_id=model_id,
-        percent_difference=percent_difference,
-        year_min=year_min,
-        year_max=year_max,
-        price_min=price_min,
-        price_max=price_max,
-        user=user
-        )
+    def create_user(self):
+        '''
+        Если пользователь уже создан return User
+        '''
+        with db:
+            if User.get(User.telegram_id == self.telegram_id):
+                return User.get(User.telegram_id == self.telegram_id)
+            else:
+                User.create(telegram_id=self.telegram_id)
+                return User.get(User.telegram_id == self.telegram_id)
+            
+    @staticmethod
+    def create_request(brand_id, model_id, percent_difference, year_min, year_max, price_min, price_max, user):
+        '''
+        Добавляем новые данные поиска для User
+        '''
+        with db:
+            Request.create(
+                brand_id=brand_id,
+                model_id=model_id,
+                percent_difference=percent_difference,
+                year_min=year_min,
+                year_max=year_max,
+                price_min=price_min,
+                price_max=price_max,
+                user=user
+                )
 
-def get_sefch_data(telegram_id):
-    '''
-    Вкрнет поисковые параметры для конкретного пользователя
-    '''
-    requests_list = []
-    user = User.get(User.telegram_id == telegram_id)
-    for request in user.requests:
-        requests_list.append({
-                'brand_id':request.brand_id,
-                'model_id':request.model_id,
-                'percent_difference':request.percent_difference,
-                'year_min':request.year_min,
-                'year_max':request.year_max,
-                'price_min':request.price_min,
-                'price_max':request.price_max
-                })
-    return requests_list
+    def get_sefch_data(self):
+        '''
+        Вкрнет поисковые параметры для конкретного пользователя
+        '''
+        with db:
+            user = User.get(User.telegram_id == self.telegram_id)
+            for request in user.requests:
+                self.requests_list.append({
+                        'brand_id':request.brand_id,
+                        'model_id':request.model_id,
+                        'percent_difference':request.percent_difference,
+                        'year_min':request.year_min,
+                        'year_max':request.year_max,
+                        'price_min':request.price_min,
+                        'price_max':request.price_max
+                        })
+        return self.requests_list
---
+++ main.py
@@ -0,0 +1,2 @@
+from api.av1 import brand, Select_car, Pars_info_id_file, Search_cars
+from api.controls import Control_db
