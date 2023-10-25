faaa934663af3fe88c0c1716cfa77c485ae906e5
MANSUXA228 <141494937+MANSUXA228@users.noreply.github.com>
2023-10-25 19:34:29 +0300

    new version 2.2.8 path 1.4.8.8

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 import asyncio, logging
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from callbacks import brand, coice, model, delete,generations
-from command import start, help, mycars
+from callbacks import brand, coice, model, delete,generations, perekup
+from command import start, help, mycars,addcars
 from fake_useragent import UserAgent as Userr
+from message import output_car
 from bs4 import BeautifulSoup as bs
 
 logging.basicConfig(level=logging.INFO)
@@ -11,7 +12,8 @@ bot = Bot(token='6315832729:AAGC6fYoRIo6QQH595zsXjgN2pZorwvDGi8')
 storage:MemoryStorage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
-dp.include_routers(brand.router, coice.router, model.router, start.router, delete.router, help.router, mycars.router,generations.router)
+dp.include_routers(brand.router, coice.router, model.router, start.router, delete.router, help.router, mycars.router,
+                   generations.router,addcars.router, perekup.router,output_car.router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
